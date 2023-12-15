907d2a53a1fba779cf5d94442ba30e667d6f5e1b
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2023-12-15 14:32:10 -0800

    betty again

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 /*
- * main - aint gonna describe anyshit
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
  *
  * Return: always 0
  */
