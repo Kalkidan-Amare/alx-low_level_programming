68c427f52f86b132bc8eb0b1929113255c4831b1
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2023-12-19 06:49:30 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ *
+ * return: always 0
+ */
+
+int main(void){
+	char iteam[]="_putchar"
+	int i;
+	for(i=0;i<8;i++){
+		_putchar(itema[i]);
+	}
+	_putchar('\n')
+			
+	return 0;
+}
