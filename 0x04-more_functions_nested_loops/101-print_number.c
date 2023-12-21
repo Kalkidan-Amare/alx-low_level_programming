f48b18cd143e932dcc416f05e3e0965f4f68613c
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2023-12-21 06:40:05 -0800

    advanced

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0){
+		n=-n;
+		_putchar('-');
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('n');
+}
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,7 +6,7 @@
  */
 void print_line(int n)
 {
-	if (size <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
