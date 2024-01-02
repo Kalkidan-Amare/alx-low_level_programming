fd1ff902f5e4e6695a1cbcb1751e075a314e1d92
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-01-02 12:05:58 -0800

    fixing

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,9 +7,10 @@
  */
 void puts2(char *str)
 {
-	while (str != '\0')
+	while (*str != '\0')
 	{
-		_putchar(*(str + 2));
+		_putchar(*str);
+		str += 2;
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src++) != '\n')
+	while (src[i] != '\0')
 		dest[i++] = src[i++];
 	dest[i] = '\0';
 	return (dest);
