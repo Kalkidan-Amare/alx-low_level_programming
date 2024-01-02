947e72458814ca36cbaca4f98ad7198de8ca51b2
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-01-02 10:48:37 -0800

    all mandatory tasks

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src++) != '\n')
+		dest[i++] = src[i++];
+	dest[i] = '\0';
+	return (dest);
+}
