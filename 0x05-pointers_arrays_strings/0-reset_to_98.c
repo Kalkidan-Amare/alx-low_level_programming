1b0cd6dabb867a04cd00044f2d48340f0c158c93
Kalkidan Amare <kalkidanamare11a@gmail.com>
2024-01-02 13:47:03 +0300

    betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98: a function that takes a pointer 
- *
- * Return: 
+ * reset_to_98 - a function that takes a pointer 
+ * @n: input
+ * Return:
  */
 
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
