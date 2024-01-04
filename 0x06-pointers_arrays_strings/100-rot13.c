9fad3f4c2437b78e0af39241f0a15c900d152760
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-01-04 11:40:03 -0800

    advanced task

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,12 +10,16 @@
 char *rot13(char *s)
 {
 	int i;
+
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	
 	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] > 'a' && s[i] < 'z' || s[i] > 'A' && s[i] < 'Z')
-		{
-			s[i] = (s[i] + 13) % 26
-		}
-	}
+		for (j = 0; j < 52; j++)
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+	return (s);
 }
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int n1 = n;
+
+	if (n1 < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	while (n1 / 10 > 0)
+		print_number(n1 / 10);
+
+	_putchar((n % 10) + '0')
+}
