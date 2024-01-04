f79209149bba30c18608607cb00555af9759f9bd
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-01-04 04:44:58 -0800

    all mandatory

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int i = 0, j;
+
+	char arr = "aAeEoOtTlL";
+	char val[] = "44007711";
+	
+	while (n[i] != '\0')
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == arr[j])
+				n[i] = arr[j];
+		}
+	}
+
+	return (n);
+}
