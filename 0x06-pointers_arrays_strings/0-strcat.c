da3ea34ba16e112eb1a020deaabf445bc3fdd76f
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-01-04 05:53:42 -0800

    fu

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,22 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates two strings
- * @dest : input
- * @src : input
- * Return: char
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
-	i = j = 0;
+	i = 0;
 	while (dest[i] != '\0')
-		i++
-
+	{
+		i++;
+	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,11 +7,12 @@
  */
 char *string_toupper(char *n)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (n[i] != '\0')
-	{	
-		if n[i] >= 'a' && n[i] <= 'z':
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
 		i++;
 	}
