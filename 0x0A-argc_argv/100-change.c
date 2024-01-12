e8c4bc026336d18e24ee495327be224d94a47db5
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-01-12 01:34:29 -0800

    optimize

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,16 +12,17 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, num, count;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	count = 0;
+	num = atoi(argv[1];
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int i;
-	int count = 0;
-	int coins[] = {25, 10, 5, 2, 1};
-	int num = atoi(argv[1];
 
 	if (num < 0)
 	{
