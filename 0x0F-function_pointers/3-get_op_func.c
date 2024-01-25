4039789463059358f6df8e0c977dcd245728458c
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-01-24 18:57:01 -0800

    fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,32 +1,39 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
+ * get_op_func - Entry point
+ * Description: selects the correct function to perform the
+ *               operation asked by the user.
+ * @s: operator passed as argument to the program.
+ *
+ * Return: a pointer to the function that corresponds to the operator
+ * given as a parameter.
+ */
+
 int (*get_op_func(char *s))(int, int)
 {
+	/* Array of structs holding function pointers */
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div },
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i = 0;
 
-	while (i < 5)
+	/* Loop through ops array until correct operator is found */
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		/* Compare operator strings */
+		if (*(ops[i].op) == *s)
+		{
+			/* Return corresponding function pointer */
 			return (ops[i].f);
-
+		}
 		i++;
 	}
-
-	return (0);
+	/* If operator not found */
+	return (NULL);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,75 +1,66 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * op_add - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_add - Entry point
+ * Description: returns the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
-  * op_sub - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_sub - Entry point
+ * Description: returns the difference of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
-  * op_mul - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_mul - Entry point
+ * Description: returns the product of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
-  * op_div - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_div - Entry point
+ * Description: returns the result of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the division of a by b
+ */
+
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
 /**
-  * op_mod - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
+ * op_mod - Entry point
+ * Description: returns the remainder of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of the division of a by b
+ */
+
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
